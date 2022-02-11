c16d1b01be0732c64630dd06d0f551deb4d981ff
Miguel Angel Muñoz Pozos <miguel.pozos@wagento.com>
2022-02-11 15:24:45 -0600

    GET: recibir parámetros

--- index.js
+++ index.js
@@ -1,15 +1,71 @@
-
 const express = require("express");
-
 const app = express();
+const PORT = 3000;
 
-
-const port = 3000;
-
-app.get ("/", (req, res) => {
-  res.send("hola mi server en express");
+app.get('/', (req, res) => {
+  res.send('Hello ');
 });
 
-app.listen(port, () => {
-  console.log("http://locahost:" + port);
+app.get('/route-new', (req, res) => {
+  res.send('Hola soy una nueva ruta o endpoint');
 });
+
+app.get('/products', (req, res) => {
+  res.json([{
+    name: 'Laptop Gamer',
+    price: 23000,
+  },
+  {
+    name: 'iPhone X3',
+    price: 32000,
+  }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'iPhone X3',
+    price: 32000,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  });
+});
+
+app.get('/categories/:categoryId', (req, res) => {
+  const { categoryId } = req.params;
+  res.json({
+    categoryId,
+    category: 'Computers & Accesories'
+  });
+});
+
+app.get('/people', (req, res) => {
+  res.json([{
+    name: 'Arturo',
+    type: 'employee'
+  }, {
+    name: 'Jimena',
+    type: 'customer'
+  }]);
+});
+
+app.get('/people/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Arturo',
+    type: 'employee'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Mi puerto es: ' + PORT);
+})
