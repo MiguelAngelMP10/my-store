0dc003af32fa55fff6e4100b51ee3d0c2f6ef564
Miguel Angel Muñoz Pozos <miguel.pozos@wagento.com>
2022-02-11 15:11:25 -0600

    Instalación de Express.js y tu primer servidor HTTP

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log('My App');
 
+const express = require("express");
+
+const app = express();
+
+
+const port = 3000;
+
+app.get ("/", (req, res) => {
+  res.send("hola mi server en express");
+});
+
+app.listen(port, () => {
+  console.log("http://locahost:" + port);
+});
