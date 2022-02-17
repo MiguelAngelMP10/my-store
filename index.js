ad7b8f713c58eb50310328549065737996a80f9f
Miguel Angel Muñoz Pozos <miguel.pozos@wagento.com>
2022-02-16 19:59:44 -0600

    Problema de CORS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const routerApi = require('./routes');
+const cors = require('cors');
+
 const { logErrors, errorHandler } = require('./middlewares/error.handler');
 
 
@@ -7,6 +9,21 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello ');
